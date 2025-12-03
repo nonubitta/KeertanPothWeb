f38e61389d888826d1abe0a498c36a02e2a6ac1c
nonubitta <nonubitta@gmail.com>
2025-12-03 11:26:17 -0800

    Update Shabad and insert new records in Writer and BaniName

--- src/assets/api/script_v15.sql
+++ src/assets/api/script_v15.sql
@@ -1,2 +1,4 @@
 Update Shabad Set shabadId = 5364 where shabadid = 555363;
+INSERT INTO Writer (WriterID,WriterEnglish,WriterGurmukhi) 	select 55, '', '' where not EXISTS (SELECT 1 from Writer WHERE writerid = 55); 
+INSERT INTO BaniName (Id,PunjabiName,EnglishName,IsVisible,Bookmark) 	Select '30', 'Ardws', 'Ardaas', 1, 0 WHERE Not EXISTS (SELECT 1 from BaniName where id = 30); 
 update DbVersion set version = 15;
