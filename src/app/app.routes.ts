3686cda69f8f488afde9f16f7a56f379ee99b7a3
codingwithdaas <codingwithdaas@gmail.com>
2026-07-13 22:55:23 -0700

    updated routes with titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+
 import { Search } from './search/search';
 import { Admin } from './admin/admin';
 import { Dashboard } from './dashboard/dashboard';
@@ -7,10 +8,10 @@ import { Support } from './support/support';
 import { PrivacyPolicy } from './privacy-policy/privacy-policy';
 
 export const routes: Routes = [
-  { path: '', component: Search },
-  { path: 'admin', component: Admin },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'pothi', component: ManagePothi },
-  { path: 'support', component: Support },
-  { path: 'privacy-policy', component: PrivacyPolicy }
+  { path: '', component: Search, title: 'Search Gurbani | Keertan Pothi' },
+  { path: 'dashboard', component: Dashboard, title: 'Dashboard | Keertan Pothi' },
+  { path: 'pothi', component: ManagePothi, title: 'Manage Pothi | Keertan Pothi' },
+  { path: 'admin', component: Admin, title: 'Admin | Keertan Pothi' },
+  { path: 'support', component: Support, title: 'Support | Keertan Pothi' },
+  { path: 'privacy-policy', component: PrivacyPolicy, title: 'Privacy Policy | Keertan Pothi' }
 ];
