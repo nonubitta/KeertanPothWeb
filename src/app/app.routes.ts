6266de924fb648430b76a95b11e061d062c84e1a
codingwithdaas <codingwithdaas@gmail.com>
2026-07-13 22:55:23 -0700

    updated routes with titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,29 @@
 import { Routes } from '@angular/router';
+
 import { Search } from './search/search';
 import { Admin } from './admin/admin';
 import { Dashboard } from './dashboard/dashboard';
 import { ManagePothi } from './manage-pothi/manage-pothi';
 
 export const routes: Routes = [
-  { path: '', component: Search },
-  { path: 'admin', component: Admin },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'pothi', component: ManagePothi }
+  {
+    path: '',
+    component: Search,
+    title: 'Search Gurbani | Keertan Pothi'
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    title: 'Dashboard | Keertan Pothi'
+  },
+  {
+    path: 'pothi',
+    component: ManagePothi,
+    title: 'Manage Pothi | Keertan Pothi'
+  },
+  {
+    path: 'admin',
+    component: Admin,
+    title: 'Admin | Keertan Pothi'
+  }
 ];
