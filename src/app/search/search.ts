7851090b46aeb3abcac9a7678ba04531b420b8b8
Gurmeet Singh Khalsa <nonubitta@gmail.com>
2026-09-07 18:43:54 -0700

    fix: change favorites addition from unshift to push to maintain order

--- src/app/search/search.ts
+++ src/app/search/search.ts
@@ -354,7 +354,7 @@ this.seo.setStructuredData({
     if (this.selectedItem && this.selectedItem.Gurmukhi) {
       // Avoid duplicates by ShabadID
       if (!this.favorites.some(f => f.VerseID === this.selectedItem!.VerseID)) {
-        this.favorites.unshift(this.selectedItem);
+        this.favorites.push(this.selectedItem);
         localStorage.setItem(this.FAVORITES_KEY, JSON.stringify(this.favorites));
         this.showRoastMessageFn('Shabad added to favorites');
       }
